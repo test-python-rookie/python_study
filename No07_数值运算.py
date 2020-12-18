43b3baf809e1d45196b03ffd4d62f8613bc43170
test-python-rookie <13929751497@163.com>
2020-12-18 11:17:06 +0800

    修改“No07_数值运算”的浮点型格式化

--- No07_数值运算.py
+++ No07_数值运算.py
@@ -2,9 +2,11 @@ a, b = 5, 3
 print("加：%d + %d = %d"%(a, b, a+b))
 print("减：%d - %d = %d"%(a, b, a-b))
 print("乘：%d * %d = %d"%(a, b, a*b))
-print("除，得到一个浮点数：%d / %d = %d"%(a, b, a/b))
+# %d带符号的整数，%g浮点数字(根据值的大小采用%e或%f)，%s字符串
+print("除，得到一个浮点数：%d / %d = %g"%(a, b, a/b))
 print("除，得到一个整数：%d // %d = %d"%(a, b, a//b))
 print("取余：%d"%a + " % " + "%d = %d"%(b, a%b))
 print("乘方：%d ** %d = %d"%(a, b, a**b))
+
 
 
