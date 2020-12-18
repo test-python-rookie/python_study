525d70e095c10827224d2e35d42ceed8ded5f45e
test-python-rookie <13929751497@163.com>
2020-12-18 10:57:35 +0800

    新增No07_数值运算

---
+++ No07_数值运算.py
@@ -0,0 +1,10 @@
+a, b = 5, 3
+print("加：%d + %d = %d"%(a, b, a+b))
+print("减：%d - %d = %d"%(a, b, a-b))
+print("乘：%d * %d = %d"%(a, b, a*b))
+print("除，得到一个浮点数：%d / %d = %d"%(a, b, a/b))
+print("除，得到一个整数：%d // %d = %d"%(a, b, a//b))
+print("取余：%d"%a + " % " + "%d = %d"%(b, a%b))
+print("乘方：%d ** %d = %d"%(a, b, a**b))
+
+
